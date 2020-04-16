d5c68bed245e5182732bdf21c36ac5e20e1b1548
seyi-adeleke <seyiadeleke42@yahoo.com>
2020-04-16 22:48:55 +0100

    create utils file

---
+++ utils.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const DISPLAY_MESSAGE = (time, weather, description, city) => `
+The current Weather recorded at ${time} in ${city} is ${weather} - ${description}
+`
+const BASE_OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+const parseUrlForCity = (city) => `${BASE_OPEN_WEATHER_URL}?q=${city}&APPID=${process.env.OPEN_WEATHER_API_KEY}`
+
+const fetchData = async (url) => {
+    const response = await axios.get(url);
+    return response.data;
+}
+
+const formatUnixDate = (date) => {
+    const unix_timestamp = date;
+    const dateObject = new Date(unix_timestamp * 1000);
+    const hours = dateObject.getHours();
+    const minutes = "0" + dateObject.getMinutes();
+    const seconds = "0" + dateObject.getSeconds();
+    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+};
+
+const getDisplayMessage = (dataObject) => {
+    const time = formatUnixDate(dataObject.time);
+    return DISPLAY_MESSAGE(time, dataObject.weather, dataObject.description, dataObject.city);
+}
+
+module.exports = {
+    parseUrlForCity,
+    fetchData,
+    formatUnixDate,
+    getDisplayMessage,
+}
